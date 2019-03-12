b5b823d6d93969a3fc45f6421f09cc0f25e7bc5c
allyjfuller <allyjfuller@gmail.com>
2019-03-12 12:05:07 -0400

    first commit

--- src/components/surprise-image.js
+++ src/components/surprise-image.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function SurpriseImage() {
-    return <img src="https://media.giphy.com/media/10dV5LFkPenFxm/giphy.gif" alt="A surprising fig" />;
+    return <img src="https://media.giphy.com/media/LrN9NbJNp9SWQ/giphy.gif" alt="A surprising fig" />;
 }
